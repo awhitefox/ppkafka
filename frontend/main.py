b26fd499b42e02dd1d948ef6883faaa1a6e11f26
awhitefox <ignat.k.150@gmail.com>
2022-10-19 00:03:29 +0300

    Add try-finally to main loop

--- frontend/main.py
+++ frontend/main.py
@@ -8,14 +8,16 @@ from producer import TelegramProducer
 logging.basicConfig(level=logging.INFO)
 
 loop = asyncio.new_event_loop()
+try:
+    prod = TelegramProducer(loop)
+    cons = TelegramConsumer(loop)
 
-prod = TelegramProducer(loop)
-cons = TelegramConsumer(loop)
+    bot.set_async_callback(prod.produce)
+    cons.set_async_callback(bot.on_result)
 
-bot.set_async_callback(prod.produce)
-cons.set_async_callback(bot.on_result)
-
-loop.create_task(prod.start())
-loop.create_task(cons.run())
-loop.create_task(bot.run())
-loop.run_forever()
+    loop.create_task(prod.start())
+    loop.create_task(cons.run())
+    loop.create_task(bot.run())
+    loop.run_forever()
+finally:
+    loop.close()
